78038107feb2074fd7428d8bb9bcd6e7f4c375c2
gustavo <gustavo_henriquesp@hotmail.com>
2015-09-02 18:20:10 -0300

    tarefa4

---
+++ tarefa4/src/tarefa4/Animal.java
@@ -0,0 +1,17 @@
+package tarefa4;
+
+public abstract class Animal {
+
+	public static void main(String[] args) {
+		String raça ="";
+		String idade="";
+		String peso="";
+				
+
+	}
+	
+	public void fazerBarulho(){
+		System.out.println("Barulho");
+	}
+
+}
