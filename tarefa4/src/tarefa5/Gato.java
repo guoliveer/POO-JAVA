1814e0f1e2744a280a193f6d6a9563e0336e3457
Gustavo Henrique de Oliveira <gustavo_henriquesp@hotmail.com>
2015-09-03 20:28:09 -0300

    Classes Gato Cachorro Extends Animal

---
+++ tarefa4/src/tarefa5/Gato.java
@@ -0,0 +1,11 @@
+package tarefa5;
+
+public class Gato extends Animal{
+
+	public Gato(String x, String y, String z) {
+		super(x, y, z);
+		System.out.println("Gato ADD");
+		// TODO Auto-generated constructor stub
+	}
+
+}
