d75b4b8d86762c7b21895edd365b48c416005df3
Gustavo Henrique de Oliveira <gustavo_henriquesp@hotmail.com>
2015-09-05 00:51:52 -0300

    Tarefas Java

---
+++ Tarefas/src/tarefa5/Cachorro.java
@@ -0,0 +1,12 @@
+package tarefa5;
+
+public class Cachorro extends Animal {
+
+	public Cachorro(String x,String y, String z) {
+		super(x,y,z);
+		System.out.println("Cachorro ADD");
+// TODO Auto-generated constructor stub
+	}
+	
+
+}
